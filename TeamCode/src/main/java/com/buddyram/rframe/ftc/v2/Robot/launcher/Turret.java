9e4ac525afa800bbc5fa9af53df57a82d27cbe15
buddyram <ramstewart@icloud.com>
2026-02-11 23:42:39 -0800

    turret power tuned

--- TeamCode/src/main/java/com/buddyram/rframe/ftc/v2/Robot/launcher/Turret.java
+++ TeamCode/src/main/java/com/buddyram/rframe/ftc/v2/Robot/launcher/Turret.java
@@ -9,6 +9,16 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class Turret extends BaseComponent<Robot> {
     public static final int MIN_ANGLE = -180;
     public static final int MAX_ANGLE = 180;
+    public static final int GEAR_TEETH = 363;
+    public static final int PULLEY_TEETH = 20;
+    public static final int ENCODER_TICKS_PER_REV = 28;
+    public static final double TICKS_PER_DEGREE = ENCODER_TICKS_PER_REV * GEAR_TEETH / (PULLEY_TEETH * 360.0);
+
+    private static final double MAX_POWER = 0.6;
+    private static final double MIN_POWER = 0.3;
+    private static final double MAX_POWER_ANGLE = 0;
+    private static final double MIN_POWER_ANGLE = 180;
+
     private final DcMotorEx motor;
 
     public Turret(Robot robot, DcMotorEx motor) {
@@ -16,17 +26,25 @@ public class Turret extends BaseComponent<Robot> {
         this.motor = motor;
     }
 
-    private void setPosition(double angle) {
-        motor.setTargetPosition((int) Math.floor(angle / 360.0 * 28 * 363 / 20));
+    private int angleToTicks(double angle) {
+        return (int) Math.floor(angle * TICKS_PER_DEGREE);
+    }
+
+    private double powerForAngleDelta(double angleDelta) {
+        double absDelta = Math.abs(angleDelta);
+        double t = Math.min(absDelta, MIN_POWER_ANGLE) / MIN_POWER_ANGLE;
+        return MAX_POWER - t * (MAX_POWER - MIN_POWER);
     }
 
     public boolean isReady() {
         return Math.abs(motor.getTargetPosition() - motor.getCurrentPosition()) < 2;
     }
 
-    public void setAngle (double newAngle) {
+    public void setAngle(double newAngle) {
         if (newAngle > MIN_ANGLE && newAngle < MAX_ANGLE) {
-            setPosition(newAngle);
+            double currentAngle = motor.getCurrentPosition() / TICKS_PER_DEGREE;
+            motor.setPower(powerForAngleDelta(newAngle - currentAngle));
+            motor.setTargetPosition(angleToTicks(newAngle));
         }
     }
 }
