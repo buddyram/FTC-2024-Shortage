0cc2ed74b792687cdc3e2f744527811fdd2add65
buddyram <ramstewart@icloud.com>
2026-02-11 22:01:53 -0800

    Update auto sequences with gate intake steps

--- TeamCode/src/main/java/com/buddyram/rframe/ftc/v2/opmodes/AutoClose30099.java
+++ TeamCode/src/main/java/com/buddyram/rframe/ftc/v2/opmodes/AutoClose30099.java
@@ -7,15 +7,15 @@ import com.buddyram.rframe.ftc.v2.NewDecodeBot.AutoSequenceConfig;
 import com.buddyram.rframe.ftc.v2.NewDecodeBot.AutoStep;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Auto Close 15", group = "Decode")
-public class AutoClose15 extends BaseOpmode {
+@Autonomous(name = "Auto Double Gate 30099", group = "Decode")
+public class AutoClose30099 extends BaseOpmode {
     @Override
     public void execute() throws RobotException, InterruptedException {
         Globals.DID_RUN_AUTO = true;
         decodeBot.runAutoSequence(new AutoSequenceConfig(
             /* shootPos */     new Vector3D(50, 84, 0),
-            /* shootHeading */ 90,//, AutoStep.OPEN_GATE
-            /* steps */        new AutoStep[]{AutoStep.SHOOT, AutoStep.MIDDLE, AutoStep.SHOOT, AutoStep.INTAKE_GATE, AutoStep.SHOOT, AutoStep.NEAR, AutoStep.SHOOT, AutoStep.FAR, AutoStep.SHOOT},
+            /* shootHeading */ 90,
+            /* steps */        new AutoStep[]{AutoStep.SHOOT, AutoStep.MIDDLE, AutoStep.OPEN_GATE, AutoStep.SHOOT, AutoStep.NEAR, AutoStep.OPEN_GATE, AutoStep.SHOOT, AutoStep.FAR, AutoStep.SHOOT},
             /* parkPos */      new Vector3D(24, 72, 0)
         ));
     }
