9b436365ae40365d95884fce9ba5cc765ac50d17
Austin ye <austinyeuni@gmail.com>
2026-08-08 11:15:18 -0700

    IntakeServolift/Intake

--- TeamCode/src/main/java/com/buddyram/rframe/ftc/v3/Robot/intake/IntakeServoLift.java
+++ TeamCode/src/main/java/com/buddyram/rframe/ftc/v3/Robot/intake/IntakeServoLift.java
@@ -1,4 +1,33 @@
 package com.buddyram.rframe.ftc.v3.Robot.intake;
 
-public class IntakeServoLift {
+import com.buddyram.rframe.BaseComponent;
+import com.buddyram.rframe.Robot;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class IntakeServoLift extends BaseComponent<Robot> {
+    public double angle;
+    private final Servo servoLeft;
+    private final Servo servoRight;
+    public final double DOWN = 0;
+    public final double MIDDLE = 0.5;
+    public final double UP = 1;
+    public IntakeServoLift(Robot robot, Servo servoLeft, Servo servoRight) {
+        super(robot);
+        this.servoLeft = servoLeft;
+        this.servoRight = servoRight;
+    }
+    public void setAngle(double newPos) {
+        this.angle = newPos;
+        this.servoLeft.setPosition(this.angle);
+        this.servoRight.setPosition(1-this.angle);
+    }
+    public void up() {
+        this.setAngle(this.UP);
+    }
+    public void middle() {
+        this.setAngle(this.MIDDLE);
+    }
+    public void down() {
+        this.setAngle(this.DOWN);
+    }
 }
